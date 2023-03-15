43f5517b4744c8bdfac427bda4f2a944f3b32a31
angeliquesaillet <angeliques1@hotmail.fr>
2023-03-15 17:32:07 +0100

    fdj

---
+++ R/automated_procedure.R
@@ -0,0 +1,15 @@
+###############################################################################
+
+# 4. Automated procedure to be applied on each gene (only first 4 for the moment)
+
+#Author : Angélique Saillet 
+#mail : angelique.saillet@etu.univ-grenoble-alpes.fr
+############################################################################## 
+
+source("../R/mutations_significance.R") #loading the previous file to recover the datasets
+
+
+automated.survival.analyze<-function(gene.code){
+  
+  
+}
